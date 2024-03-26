17fbd4c5747c79c52f40f5a4eb6d0692dcb4ed92
RAGAVE.I <95114993+Ragave103@users.noreply.github.com>
2024-03-26 14:03:30 +0530

    Update 2.0toDdigits.java

--- Code/2.0toDdigits.java
+++ Code/2.0toDdigits.java
@@ -1,3 +1,9 @@
+// Problem: Given a positive integer n and another positive integer 
+// d, determine whether all the digits in n are less than or equal to d.
+
+// Input: The program takes two integers n and d as input from the user.
+
+// Output: Print "yes" if all the digits in n are less than or equal to d, otherwise print "no".
 package Ragave_CodeJourney.Code;
 import java.util.*;
 class Ddigits {

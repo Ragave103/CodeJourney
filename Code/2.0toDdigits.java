fde471770a5db5a5668be85d668e1a14f64ba675
RAGAVE.I <95114993+Ragave103@users.noreply.github.com>
2024-03-26 12:13:20 +0530

    Second file

---
+++ Code/2.0toDdigits.java
@@ -0,0 +1,26 @@
+package Ragave_CodeJourney.Code;
+import java.util.*;
+class Ddigits {
+  public static void main(String[] args) {
+    int d,n,r,flag=0;
+    Scanner sc = new Scanner(System.in);
+    n= sc.nextInt();
+    d=sc.nextInt();
+    while(n!=0)
+    {
+      r=n%10;
+      if(r>d)
+      {
+        flag = 1;
+        break;
+      }
+      n/=10;
+
+    }
+    if(flag!=1)
+    System.out.println("yes");
+    else
+    System.out.println("no");
+
+  }
+}
